70d4bfb01f5c07a499a5c5cb5bdb11f5cbc11e3c
hisaki <alltimelow2011@gmail.com>
2023-09-11 07:41:05 +0900

    useState text boolean

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,15 +6,33 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(
     (e) => {
-      if (count < 10) {
-        setCount((count) => count + 1);
-      }
-    },
-    [count],
-  );
+      setCount((count) => count + 1);
+      return;
+    },[count]);
+
+  const handleDisplay = useCallback(
+    () => {
+      setIsShow((isShow) => {
+        // if (isShow) {
+        //   return false;
+        // }
+        // return isShow ? false : true;
+        return !isShow;
+      });
+    }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内の入力");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -27,8 +45,13 @@ export default function Home() {
     <>
       <div className={styles.main}>
         <Header />
-        <h1>{count}</h1>
+        {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>ボタン</button>
+        <button
+          onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示"}
+        </button>
+        <input type="text" value={text} onChange={handleChange} />
         <main>
           <Main
             title="個人開発用サイト"
