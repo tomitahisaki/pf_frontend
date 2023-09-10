489fbc4518be3ccf7586a75e8e4a29f5d1d2e1b4
hisaki <alltimelow2011@gmail.com>
2023-09-10 10:16:53 +0900

    usecallback

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,26 @@ import styles from "@/styles/Home.module.scss";
 import { Main } from "@/components/Main";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+  
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+  
   return (
     <>
       <Header />
+      <a 
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <main className={`${styles.main}`}>
         <Main
           title="個人開発用サイト"
