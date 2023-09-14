1d5c5d4fe1e47d831d9b6ffbb37c64ab1515bb9a
hisaki <alltimelow2011@gmail.com>
2023-09-14 20:12:50 +0900

    get artists.images from spotify

--- src/pages/spotify.jsx
+++ src/pages/spotify.jsx
@@ -2,6 +2,7 @@ import styles from "@/styles/Home.module.scss";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Spotify() {
   const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -10,26 +11,51 @@ export default function Spotify() {
   const RESPONCE_TYPE = "token"
 
   const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
 
   useEffect(() => {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token")
-    console.log("hash", hash)
-    console.log("token", token)
 
     if (!token && hash) {
       token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
       window.location.hash = ""
       window.localStorage.setItem("token", token);
-      setToken(token);
     }
+
+    setToken(token);
     
   }, [])
 
   const logout = () => {
     setToken("");
     window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div key={artist.id}>
+        {artist.images.length ? <img witdh={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div> }
+        {artist.name}
+      </div>
+    ))
   }
 
   return (
@@ -41,6 +67,15 @@ export default function Spotify() {
           <a href={`${SPOTIFY_AUTH_ENDPOINT}?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONCE_TYPE}`}>Login to spotify</a>
         : <button onClick={logout}>Logout</button> }
 
+        { token ?
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+            <button type={"submit"}>Search</button>
+          </form>
+
+          : <h2>Please Login</h2> }
+
+          {renderArtists()}
         <Footer />
       </div>
     </>
