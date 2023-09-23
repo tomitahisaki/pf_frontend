3cce7490fe7f04b54f32e82a1a5c9a498aa4bbdb
hisaki <alltimelow2011@gmail.com>
2023-09-23 21:01:21 +0900

    add env

--- src/components/Todo/EditTodoForm.jsx
+++ src/components/Todo/EditTodoForm.jsx
@@ -6,19 +6,44 @@ import Button from "@mui/joy/Button";
 import Textarea from "@mui/joy/Textarea";
 import SelectSmall from "@/components/Layouts/selectBox";
 
-export function CreateTodoForm() {
+export function EditTodo(props) {
+  const [id, setId] = useState();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [status, setStatus] = useState();
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    const getTodo = async () => {
+      try {
+        // const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_API}/todos/${todoId}`);
+        //
+        // const { title, description, status } = res.deta;
+        const { id, title, description, status } = props.todoDetail;
+        setId(id);
+        setTitle(title);
+        setDescription(description);
+        setStatus(status);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (id) {
+      getTodo();
+    }
+  }, []);
+
+  if (!id) {
+    return <div>Loading...</div>;
+  }
 
   const handleValueChange = (newValue) => {
     setStatus(newValue);
-    console.log(status);
   };
 
   const handleSubmit = async () => {
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_BASE_API}/todos`, {
+      await axios.put(`http://localhost:3010/todos${id}`, {
         todo: {
           title,
           description,
@@ -27,7 +52,7 @@ export function CreateTodoForm() {
       });
       setTitle("");
       setDescription("");
-      setStatus();
+      setStatus("");
 
       window.location.reload();
     } catch (error) {
